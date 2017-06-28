4c8cc746053d315db34bfb671f6aabce8a5568ad
debgolick <shock1142003@atlanticbb.net>
2017-06-28 10:17:29 -0400

    divison

---
+++ Division.rb
@@ -0,0 +1,5 @@
+def division(number10, number5)
+	return number10 / number5
+end
+puts division(20, 2)
+#puts division(15,3)
---
+++ multiplcation.rb
@@ -0,0 +1,6 @@
+def multiplcation(number2, number4)
+ return number2 * number4
+ end
+ puts multiplcation(10, 5)
+ #puts multiplcation(5,4)
+ 
